311bc5e32de162bd9a58a2f2c403b1eb77614ea2
marquem146 <fsouzasrs@gmail.com>
2026-08-06 08:51:15 -0300

    commit

--- servidor_Main/servidor.js
+++ servidor_Main/servidor.js
@@ -1,12 +1,18 @@
-﻿// Importando o módulo http
+﻿
 const http = require('http');
-// Criando o servidor
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname,'html','index.html'));
+});
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
 res.end('Hello, World!\n');
 });
-// Especificando a porta
+
 const port = 3000;
 server.listen(port, () => {
 console.log(`Servidor rodando em http://localhost:${port}/`);
