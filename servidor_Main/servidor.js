516e62045bc7960bafc7f2c2d5970e37872ef994
marquem146 <fsouzasrs@gmail.com>
2026-08-07 11:09:56 -0300

    servidor zero bala

--- servidor_Main/servidor.js
+++ servidor_Main/servidor.js
@@ -7,13 +7,9 @@ const app = express();
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname,'html','index.html'));
 });
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello, World!\n');
-});
+
 
 const port = 3000;
-server.listen(port, () => {
+app.listen(port, () => {
 console.log(`Servidor rodando em http://localhost:${port}/`);
 });
