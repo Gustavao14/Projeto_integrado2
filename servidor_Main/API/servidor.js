0d91d71aa45b667efaf4b10a9ac64c967801995c
marquem146 <fsouzasrs@gmail.com>
2026-08-10 09:46:12 -0300

    vercel teste

---
+++ servidor_Main/API/servidor.js
@@ -0,0 +1,22 @@
+﻿
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(process.cwd(),'public')));
+
+app.get('/', (req, res) => {
+res.sendFile(process.cwd(),'servidor_Main','html','Home.html');
+});
+
+app.get('/IA', (req,res) =>{
+res.sendFile(process.cwd(),'servidor_Main','html','IA.html');
+});
+
+app.use((req,res) => {
+res.status(404).json({
+erro:"Página não encontrada"
+});
+});
+
+module.exports = app;
